0a98b3a7bf2fff7ef1bf82e66d3cfad2e587f8fa
petros <pit.trak@gmail.com>
2020-07-30 17:51:07 +0300

    Added yargs module

--- notes App/app.js
+++ notes App/app.js
@@ -1,12 +1,59 @@
 const validator = require('validator')
 const chalk  = require('chalk')
+const yargs = require('yargs')
 
-const command = process.argv[2]
+// Customize yargs version
 
-if(command === 'add'){
-    console.log('adding note');
-} else if (command === 'remove'){
-    console.log('removing note');
-}
+yargs.version('1.1.0')
 
-console.log(process.argv);
+// add, remove, read, list
+
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv)=>{
+        console.log('Title:', argv.body);
+    }
+})
+
+// create a remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: ()=> {
+        console.log('Removing a note');
+    }
+})
+
+// create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: ()=> {
+        console.log('Reading a note')
+    }
+})
+
+// create a list command
+yargs.command({
+    command: 'list',
+    describe: 'Listing all notes',
+    handler: ()=> {
+        console.log('All notes');
+    }
+})
+
+yargs.parse()
