936cd086a0a4214112c037672c2da8845616c7bf
petros <pit.trak@gmail.com>
2020-07-30 14:41:52 +0300

    explored chalk module

--- notes App/app.js
+++ notes App/app.js
@@ -1,7 +1,17 @@
 const validator = require('validator')
-const getNotes = require('./notes.js');
- 
-const message = getNotes('Suprise motherfucker!')
 
-console.log(validator.isEmail('pit.trak@gmail.com'));
-console.log(validator.isURL('https://www.google.com'))
+const chalk  = require('chalk')
+const log = console.log
+const error = chalk.bold.red
+const warning = chalk.keyword('orange')
+const success = chalk.bold.green
+
+// const getNotes = require('./notes.js');
+ 
+// const message = getNotes('Suprise motherfucker!')
+
+// console.log(validator.isEmail('pit.trak@gmail.com'));
+// console.log(validator.isURL('https://www.google.com'))
+
+log(chalk.green.underline.bold('Success') + chalk.red('!'))
+log(success('Success!'))
