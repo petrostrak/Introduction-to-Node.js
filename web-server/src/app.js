cff5a51e06c8c474e71301f679eea6fce929f76d
petros <pit.trak@gmail.com>
2020-08-01 15:51:31 +0300

    The Query String

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -39,14 +39,29 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-    res.send([{
-        location: {
-            lat: 40,
-            lon: -73
-        },forecast: {
-            msg: ''
-        }
-    }])
+    if(!req.query.address){
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Athens',
+        address: req.query.address
+    })
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search){
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
 })
 
 app.get('/help/*', (req, res) => {
