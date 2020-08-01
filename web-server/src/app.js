76548df31158725ba932d0a62228c9695985abfb
petros <pit.trak@gmail.com>
2020-08-01 12:46:26 +0300

    Dynamic Pages with Templating and Handlebars (template engines)

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,21 +4,28 @@ const express = require('express')
 const app =  express()
 const publicDir  = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDir))
 
-// app.get('/help', (req, res) =>{
-//     res.send([{
-//         name: 'Petros',
-//         age: 34
-//     },{
-//         profession: 'Web Developer',
-//         education: 'Mechanical Engineer'
-//     }])
-// })
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Petros'
+    })
+})
 
-// app.get('/about', (req, res) => {
-//     res.send('<h1>About</h1>')
-// })
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Petros'        
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        msg: 'This is a help message'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send([{
@@ -34,3 +41,18 @@ app.get('/weather', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is up on port: 3000');
 })
+
+
+// app.get('/help', (req, res) =>{
+//     res.send([{
+//         name: 'Petros',
+//         age: 34
+//     },{
+//         profession: 'Web Developer',
+//         education: 'Mechanical Engineer'
+//     }])
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send('<h1>About</h1>')
+// })
