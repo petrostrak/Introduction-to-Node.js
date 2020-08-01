c9a9ee9d905f4654e2aa9369f903a4cedcea7aca
petros <pit.trak@gmail.com>
2020-08-01 14:13:02 +0300

    Advanced Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,25 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app =  express()
 
 // Define paths for Express config
 const publicDir  = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, './templates')
+const viewsPath = path.join(__dirname, './templates/views')
+const partialsPath = path.join(__dirname, './templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
-app.use(express.static(publicDir))
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
+app.use(express.static(publicDir))
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather App',
-        name: 'Petros'
+        title: 'Weather',
+        name: 'Petros Trak'
     })
 })
 
@@ -29,7 +32,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        msg: 'This is a help message'
+        msg: 'This is a help message',
+        title: 'Help',
+        name: 'Petros'  
     })
 })
 
