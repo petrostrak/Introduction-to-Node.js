7318d2f6450ebf7e48cf9d3c0bb0943cbac37d5b
petros <pit.trak@gmail.com>
2020-08-02 15:05:25 +0300

    Browser HTTP Requests with Fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,11 @@
-console.log('Not a meaningful js message');
+fetch('http://localhost:3000/weather?address=tokyo')
+    .then((response) => {
+        response.json().then((data) => {
+            if(data.error){
+                console.log(data.error);
+            } else {
+                console.log(data.location);
+                console.log(data.forecast);
+            }
+        })
+    })
